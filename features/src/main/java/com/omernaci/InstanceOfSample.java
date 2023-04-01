9e8d7a000714ceef58719a170fbd7abf91d24265
omernaci <soydemir.naci@gmail.com>
2023-04-01 19:17:02 +0300

    instanceof updated

--- features/src/main/java/com/omernaci/InstanceOfSample.java
+++ features/src/main/java/com/omernaci/InstanceOfSample.java
@@ -1,24 +1,25 @@
 package com.omernaci;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 public class InstanceOfSample {
 
     public static void main(String[] args) {
+        Object obj1 = "omernaci";
+        Object obj2 = 42;
+        Object obj3 = new Object();
 
-        List<String> stringList = new ArrayList<>();
+        processObject(obj1); // Output: Input is a string: Hello
+        processObject(obj2); // Output: Input is an integer: 42
+        processObject(obj3); // Output: Input is of unknown type
+    }
 
-        if (stringList instanceof LinkedList) {
-            System.out.println("old style");
+    public static void processObject(Object obj) {
+        if (obj instanceof String str) {
+            System.out.println("Input is a string: " + str);
+        } else if (obj instanceof Integer i) {
+            System.out.println("Input is an integer: " + i);
+        } else {
+            System.out.println("Input is of unknown type");
         }
-
-        if (stringList instanceof ArrayList a) {
-            System.out.println(a.hashCode());
-        }
-
-
     }
 
 }
